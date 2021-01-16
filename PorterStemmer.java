cee7e17589c3ae3928eaca2d548f6417e56262d4
Dulan Jayasuriya <dulanjayasuriya@gmail.com>
2021-01-16 12:57:27 +0530

    Porter Stemmer Improvements - More rules added

--- PorterStemmer.java
+++ PorterStemmer.java
@@ -1,6 +1,7 @@
 public class PorterStemmer {
     String stem(String token){
         String pre_tocken = token;
+        boolean step2_in_1b = false;
         if(token.endsWith("sses")){
             token = token.substring(0, token.length()-2);
         }
@@ -18,7 +19,31 @@ public class PorterStemmer {
         }
         else if(token.endsWith("ed")){
             token = token.substring(0, token.length()-2);
+            step2_in_1b = true;
         }
+        else if(token.endsWith("ing")){
+            token = token.substring(0, token.length()-3);
+            step2_in_1b = true;
+        }
+
+        if(step2_in_1b){
+            if(token.endsWith("at")){
+                token = token + 'e';
+            }
+            else if(token.endsWith("bl")){
+                token = token + 'e';
+            }
+            else if(token.endsWith("iz")){
+                token = token + 'e';
+            }
+            else if(token.endsWith("s")){
+                token = token.substring(0, token.length()-1);
+            }
+            else if(token.endsWith("eed")){
+                token = token.substring(0, token.length()-1);
+            }
+        }
+
         if(!pre_tocken.equals(token)){
             System.out.println("Porter stemmer convert '" + pre_tocken + "' to '" + token + "'");
         }
